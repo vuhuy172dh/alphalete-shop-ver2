3cc61a3570f92580347dd3965c5aab623ca6868a
Vu_Huy <quochuy123dh@gmail.com>
2022-08-16 00:02:18 +0700

    update trending component

--- components/trending.tsx
+++ components/trending.tsx
@@ -28,10 +28,14 @@ const Trending = () => {
           <TrendingItem index={0}>
             <ThumbnailsCarousel
               sources={[
-                '../assets/media-1.jpeg',
-                '../assets/media-2.jpeg',
-                '../assets/media-3.jpeg',
-                '../assets/media-4.jpeg'
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310133_80x.jpg?v=1654661505',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/EmptyName6_80x.jpg',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079_80x.jpg?v=1654662422',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310027_80x.jpg?v=1654661653',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079choc_80x.jpg?v=1654662640',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310172_80x.jpg?v=1654661674',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay314312_027f2867-ae60-4b31-8e67-2d761bb3d80f_80x.jpg?v=1654879164',
+                '//cdn.shopify.com/s/files/1/0667/0133/products/AmplifyMay3060_80x.jpg?v=1651809749'
               ]}
               names={['image', 'image', 'image', 'image']}
               product_name="Ozone Short"
@@ -43,10 +47,14 @@ const Trending = () => {
           <TrendingItem index={0}>
             <ThumbnailsCarousel
               sources={[
-                '../assets/media-1.jpeg',
-                '../assets/media-2.jpeg',
-                '../assets/media-3.jpeg',
-                '../assets/media-4.jpeg'
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310133_80x.jpg?v=1654661505',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/EmptyName6_80x.jpg',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079_80x.jpg?v=1654662422',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310027_80x.jpg?v=1654661653',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079choc_80x.jpg?v=1654662640',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310172_80x.jpg?v=1654661674',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay314312_027f2867-ae60-4b31-8e67-2d761bb3d80f_80x.jpg?v=1654879164',
+                '//cdn.shopify.com/s/files/1/0667/0133/products/AmplifyMay3060_80x.jpg?v=1651809749'
               ]}
               names={['image', 'image', 'image', 'image']}
               product_name="Ozone Short"
@@ -58,10 +66,14 @@ const Trending = () => {
           <TrendingItem index={0}>
             <ThumbnailsCarousel
               sources={[
-                '../assets/media-1.jpeg',
-                '../assets/media-2.jpeg',
-                '../assets/media-3.jpeg',
-                '../assets/media-4.jpeg'
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310133_80x.jpg?v=1654661505',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/EmptyName6_80x.jpg',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079_80x.jpg?v=1654662422',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310027_80x.jpg?v=1654661653',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079choc_80x.jpg?v=1654662640',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310172_80x.jpg?v=1654661674',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay314312_027f2867-ae60-4b31-8e67-2d761bb3d80f_80x.jpg?v=1654879164',
+                '//cdn.shopify.com/s/files/1/0667/0133/products/AmplifyMay3060_80x.jpg?v=1651809749'
               ]}
               names={['image', 'image', 'image', 'image']}
               product_name="Ozone Short"
@@ -73,10 +85,14 @@ const Trending = () => {
           <TrendingItem index={0}>
             <ThumbnailsCarousel
               sources={[
-                '../assets/media-1.jpeg',
-                '../assets/media-2.jpeg',
-                '../assets/media-3.jpeg',
-                '../assets/media-4.jpeg'
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310133_80x.jpg?v=1654661505',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/EmptyName6_80x.jpg',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079_80x.jpg?v=1654662422',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310027_80x.jpg?v=1654661653',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079choc_80x.jpg?v=1654662640',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310172_80x.jpg?v=1654661674',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay314312_027f2867-ae60-4b31-8e67-2d761bb3d80f_80x.jpg?v=1654879164',
+                '//cdn.shopify.com/s/files/1/0667/0133/products/AmplifyMay3060_80x.jpg?v=1651809749'
               ]}
               names={['image', 'image', 'image', 'image']}
               product_name="Ozone Short"
@@ -88,10 +104,14 @@ const Trending = () => {
           <TrendingItem index={0}>
             <ThumbnailsCarousel
               sources={[
-                '../assets/media-1.jpeg',
-                '../assets/media-2.jpeg',
-                '../assets/media-3.jpeg',
-                '../assets/media-4.jpeg'
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310133_80x.jpg?v=1654661505',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/EmptyName6_80x.jpg',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079_80x.jpg?v=1654662422',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310027_80x.jpg?v=1654661653',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079choc_80x.jpg?v=1654662640',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310172_80x.jpg?v=1654661674',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay314312_027f2867-ae60-4b31-8e67-2d761bb3d80f_80x.jpg?v=1654879164',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyMay3060_80x.jpg?v=1651809749'
               ]}
               names={['image', 'image', 'image', 'image']}
               product_name="Ozone Short"
@@ -103,10 +123,14 @@ const Trending = () => {
           <TrendingItem index={0}>
             <ThumbnailsCarousel
               sources={[
-                '../assets/media-1.jpeg',
-                '../assets/media-2.jpeg',
-                '../assets/media-3.jpeg',
-                '../assets/media-4.jpeg'
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310133_80x.jpg?v=1654661505',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/EmptyName6_80x.jpg',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079_80x.jpg?v=1654662422',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310027_80x.jpg?v=1654661653',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079choc_80x.jpg?v=1654662640',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310172_80x.jpg?v=1654661674',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay314312_027f2867-ae60-4b31-8e67-2d761bb3d80f_80x.jpg?v=1654879164',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyMay3060_80x.jpg?v=1651809749'
               ]}
               names={['image', 'image', 'image', 'image']}
               product_name="Ozone Short"
@@ -118,10 +142,14 @@ const Trending = () => {
           <TrendingItem index={0}>
             <ThumbnailsCarousel
               sources={[
-                '../assets/media-1.jpeg',
-                '../assets/media-2.jpeg',
-                '../assets/media-3.jpeg',
-                '../assets/media-4.jpeg'
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310133_80x.jpg?v=1654661505',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/EmptyName6_80x.jpg',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079_80x.jpg?v=1654662422',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310027_80x.jpg?v=1654661653',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079choc_80x.jpg?v=1654662640',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310172_80x.jpg?v=1654661674',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay314312_027f2867-ae60-4b31-8e67-2d761bb3d80f_80x.jpg?v=1654879164',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyMay3060_80x.jpg?v=1651809749'
               ]}
               names={['image', 'image', 'image', 'image']}
               product_name="Ozone Short"
@@ -133,10 +161,14 @@ const Trending = () => {
           <TrendingItem index={0}>
             <ThumbnailsCarousel
               sources={[
-                '../assets/media-1.jpeg',
-                '../assets/media-2.jpeg',
-                '../assets/media-3.jpeg',
-                '../assets/media-4.jpeg'
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310133_80x.jpg?v=1654661505',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/EmptyName6_80x.jpg',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079_80x.jpg?v=1654662422',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310027_80x.jpg?v=1654661653',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079choc_80x.jpg?v=1654662640',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310172_80x.jpg?v=1654661674',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay314312_027f2867-ae60-4b31-8e67-2d761bb3d80f_80x.jpg?v=1654879164',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyMay3060_80x.jpg?v=1651809749'
               ]}
               names={['image', 'image', 'image', 'image']}
               product_name="Ozone Short"
@@ -148,10 +180,14 @@ const Trending = () => {
           <TrendingItem index={0}>
             <ThumbnailsCarousel
               sources={[
-                '../assets/media-1.jpeg',
-                '../assets/media-2.jpeg',
-                '../assets/media-3.jpeg',
-                '../assets/media-4.jpeg'
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310133_80x.jpg?v=1654661505',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/EmptyName6_80x.jpg',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079_80x.jpg?v=1654662422',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310027_80x.jpg?v=1654661653',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079choc_80x.jpg?v=1654662640',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310172_80x.jpg?v=1654661674',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay314312_027f2867-ae60-4b31-8e67-2d761bb3d80f_80x.jpg?v=1654879164',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyMay3060_80x.jpg?v=1651809749'
               ]}
               names={['image', 'image', 'image', 'image']}
               product_name="Ozone Short"
@@ -163,10 +199,14 @@ const Trending = () => {
           <TrendingItem index={0}>
             <ThumbnailsCarousel
               sources={[
-                '../assets/media-1.jpeg',
-                '../assets/media-2.jpeg',
-                '../assets/media-3.jpeg',
-                '../assets/media-4.jpeg'
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310133_80x.jpg?v=1654661505',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/EmptyName6_80x.jpg',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079_80x.jpg?v=1654662422',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310027_80x.jpg?v=1654661653',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310079choc_80x.jpg?v=1654662640',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay310172_80x.jpg?v=1654661674',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyRestockMay314312_027f2867-ae60-4b31-8e67-2d761bb3d80f_80x.jpg?v=1654879164',
+                'https://cdn.shopify.com/s/files/1/0667/0133/products/AmplifyMay3060_80x.jpg?v=1651809749'
               ]}
               names={['image', 'image', 'image', 'image']}
               product_name="Ozone Short"
