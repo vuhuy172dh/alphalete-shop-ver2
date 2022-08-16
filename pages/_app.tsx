b66a9ccc215d94e24987114570a87fc19a8cb673
Vu_Huy <quochuy123dh@gmail.com>
2022-08-16 22:33:43 +0700

    update darkmode for all pages of web app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,35 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
 import ScrollObserver from '../utils/scroll-observer';
 import '@fortawesome/fontawesome-svg-core/styles';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import NavDrawer from '../components/nav-drawer';
 import Navbar from '../components/navbar';
 import NavMode from '../utils/navbar-controller';
+import DarkMode from '../utils/dark-mode';
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ScrollObserver>
-      <NavMode>
-        <Navbar />
-        <NavDrawer />
-      </NavMode>
-      <Component {...pageProps} />
-    </ScrollObserver>
-  );
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+  else {
+    return (
+      <ScrollObserver>
+        <DarkMode>
+          <NavMode>
+            <Navbar />
+            <NavDrawer />
+          </NavMode>
+          <Component {...pageProps} />
+        </DarkMode>
+      </ScrollObserver>
+    );
+  }
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,18 @@
 import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
 import Footer from '../components/footer';
 import Header from '../components/header';
 import SlideShow from '../components/slideshow';
 import Trending from '../components/trending';
-import DarkMode from '../utils/dark-mode';
 
 const Home: NextPage = () => {
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
-  else {
     return (
-      <DarkMode>
         <div>
           <Header title="Alphalete" />
           <SlideShow />
           <Trending />
           <Footer />
         </div>
-      </DarkMode>
     );
-  }
 };
 
 export default Home;
