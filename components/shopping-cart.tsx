31522339dd8dc6169f80e6d76bc0a59a9c9ca88c
Vu_Huy <quochuy123dh@gmail.com>
2022-08-21 21:47:03 +0700

    update shopping cart

--- components/shopping-cart.tsx
+++ components/shopping-cart.tsx
@@ -1,15 +1,26 @@
 import { useContext } from 'react';
 import { CartContext } from '../utils/cart-controller';
+import CartMenu from './cart-menu';
+import Trending from './trending';
 
 const ShoppingCart = () => {
-  const { cartMode } = useContext(CartContext);
+  const { cartMode, handleMode } = useContext(CartContext);
   return (
     <div
       className={`${
-        cartMode === 'open' ? 'flex' : 'hidden'
-      } fixed top-0 z-50 w-screen h-screen`}
+        cartMode === 'open'
+          ? 'z-50 visible opacity-100'
+          : 'z-[-1] invisible opacity-0'
+      } fixed top-0 w-full h-screen flex backdrop-blur-lg transition-all bg-primary_60 dark:bg-secondary_60 cursor-pointer`}
     >
-      <div></div>
+      <div className="grow flex items-center relative" onClick={handleMode}>
+        <div>
+          <Trending />
+        </div>
+      </div>
+      <div className="w-[26rem] relative">
+        <CartMenu cartItems={[]} />
+      </div>
     </div>
   );
 };
