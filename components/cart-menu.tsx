d4135ff4b3de024cb162a2f0094388b650cf87b6
Vu_Huy <quochuy123dh@gmail.com>
2022-08-21 22:50:03 +0700

    update shopping cart

--- components/cart-menu.tsx
+++ components/cart-menu.tsx
@@ -1,35 +1,35 @@
 import Link from 'next/link';
 
 interface Props {
-  cartItems: string[]
+  cartItems: string[];
 }
 
 const CartMenu = ({ cartItems }: Props) => {
   return (
-    <div className="w-full h-full right-0 flex items-center">
-      <div className="w-full h-[85%] bg-red-900 bg-primary dark:bg-secondary border border-secondary_20 dark:border-primary_20 rounded-l-2xl cursor-default">
-        {cartItems.length !== 0 ? <div>this is cart item</div> : (
-          <div className="w-full h-full flex flex-col items-center justify-center gap-3">
-            <p className="leading-none text-[1.2rem] font-bold">
-              GIVE YOUR BAG SOME LOVE
-            </p>
-            <Link href="/">
-              <a className="p-3 border-[2px] border-secondary_30 dark:border-primary_30 rounded-lg hover:border-secondary_50 dark:hover:border-primary_50 hover:bg-secondary_30 dark:hover:bg-primary_30">
-                <p className="leading-none text-[0.9rem] font-bold">
-                  WOMEN'S BEST SELLERS
-                </p>
-              </a>
-            </Link>
-            <Link href="/">
-              <a className="p-3 border-[2px] border-secondary_30 dark:border-primary_30 rounded-lg hover:border-secondary_50 dark:hover:border-primary_50 hover:bg-secondary_30 dark:hover:bg-primary_30">
-                <p className="leading-none text-[0.9rem] font-bold">
-                  MEN'S BEST SELLERS
-                </p>
-              </a>
-            </Link>
-          </div>
-        )}
-      </div>
+    <div className="w-full h-full flex items-center bg-primary dark:bg-secondary border border-secondary_20 dark:border-primary_20 rounded-l-2xl cursor-default">
+      {cartItems.length !== 0 ? (
+        <div>this is cart item</div>
+      ) : (
+        <div className="w-full h-full flex flex-col items-center justify-center gap-3">
+          <p className="leading-none text-[1.2rem] font-bold">
+            GIVE YOUR BAG SOME LOVE
+          </p>
+          <Link href="/">
+            <a className="p-3 border-[2px] border-secondary_30 dark:border-primary_30 rounded-lg hover:border-secondary_50 dark:hover:border-primary_50 hover:bg-secondary_30 dark:hover:bg-primary_30">
+              <p className="leading-none text-[0.9rem] font-bold">
+                WOMEN'S BEST SELLERS
+              </p>
+            </a>
+          </Link>
+          <Link href="/">
+            <a className="p-3 border-[2px] border-secondary_30 dark:border-primary_30 rounded-lg hover:border-secondary_50 dark:hover:border-primary_50 hover:bg-secondary_30 dark:hover:bg-primary_30">
+              <p className="leading-none text-[0.9rem] font-bold">
+                MEN'S BEST SELLERS
+              </p>
+            </a>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
--- components/shopping-cart.tsx
+++ components/shopping-cart.tsx
@@ -7,18 +7,19 @@ const ShoppingCart = () => {
   const { cartMode, handleMode } = useContext(CartContext);
   return (
     <div
-      className={`${
-        cartMode === 'open'
+      className={`${cartMode === 'open'
           ? 'z-50 visible opacity-100'
           : 'z-[-1] invisible opacity-0'
-      } fixed top-0 w-full h-screen flex backdrop-blur-lg transition-all bg-primary_60 dark:bg-secondary_60 cursor-pointer`}
+        } fixed top-0 w-full h-screen flex items-center transition-all cursor-pointer`}
     >
-      <div className="grow flex items-center relative" onClick={handleMode}>
-        <div>
-          <Trending />
-        </div>
+      <div
+        className="w-full h-screen fixed top-0 left-0 backdrop-blur-lg bg-primary_60 dark:bg-secondary_60 z-10"
+        onClick={handleMode}
+      ></div>
+      <div className="grow flex h-fit items-center relative z-20">
+        <Trending />
       </div>
-      <div className="w-[26rem] relative">
+      <div className="w-[26rem] h-[87%] relative z-20">
         <CartMenu cartItems={[]} />
       </div>
     </div>
